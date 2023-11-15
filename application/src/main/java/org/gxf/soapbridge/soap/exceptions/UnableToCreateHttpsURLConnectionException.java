d57414836915b7cc0c259e89b2843646588c7ee8
Sander Verbruggen <sander.verbruggen@alliander.com>
2023-11-15 11:07:37 +0100

    FDP-94: fix
    Signed-off-by: Sander Verbruggen <sander.verbruggen@alliander.com>

--- application/src/main/java/org/gxf/soapbridge/soap/exceptions/UnableToCreateHttpsURLConnectionException.java
+++ application/src/main/java/org/gxf/soapbridge/soap/exceptions/UnableToCreateHttpsURLConnectionException.java
@@ -3,12 +3,14 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.gxf.soapbridge.soap.exceptions;
 
+import java.io.Serial;
+
 public class UnableToCreateHttpsURLConnectionException extends ProxyServerException {
 
-  /** Serial Version UID. */
-  private static final long serialVersionUID = -8807766325167125880L;
+  @Serial private static final long serialVersionUID = -8807766325167125880L;
 
-  public UnableToCreateHttpsURLConnectionException(final String message) {
-    super(message);
+  public UnableToCreateHttpsURLConnectionException(
+      final String message, final Throwable throwable) {
+    super(message, throwable);
   }
 }
