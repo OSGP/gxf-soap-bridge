8e8732f66c2768419c76c37404c132fc8639adc5
Jasper Kamerling <jasper.kamerling@alliander.com>
2023-11-16 10:24:00 +0100

    FDP-94: cleanup SecurityConfiguration
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

--- application/src/main/kotlin/org/gxf/soapbridge/configuration/SecurityConfiguration.kt
+++ application/src/main/kotlin/org/gxf/soapbridge/configuration/SecurityConfiguration.kt
@@ -13,31 +13,28 @@ import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfiguration {
+
     @Bean
-    @Throws(Exception::class)
-    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+    fun filterChain(http: HttpSecurity): SecurityFilterChain =
         http.authorizeHttpRequests {
             it
                 .anyRequest().authenticated()
-        }
-        http.x509 {
+        }.x509 {
             it
                 .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
                 .userDetailsService(userDetailsService())
-        }
-        http.csrf { it.disable() }
-        return http.build()
-    }
+        }.csrf { it.disable() }
+            .build()
+
 
     /**
      * Uses the CN of the client certificate as the username for Springs Principal object
      */
     @Bean
-    fun userDetailsService(): UserDetailsService {
-        return UserDetailsService { username ->
+    fun userDetailsService(): UserDetailsService =
+        UserDetailsService { username ->
             return@UserDetailsService User(
                 username, "", emptyList()
             )
         }
-    }
 }
